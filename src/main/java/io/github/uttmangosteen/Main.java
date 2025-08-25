c937efa38f728956d1c7d07f05da7d159b0c5564
uttmangosteen <noisimportantpoint@gmail.com>
2025-08-26 04:01:54 +0900

    グラフの作成、解答出力部分ほぼ完成

--- src/main/java/io/github/uttmangosteen/Main.java
+++ src/main/java/io/github/uttmangosteen/Main.java
@@ -1,17 +1,33 @@
 package io.github.uttmangosteen;
 
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static io.github.uttmangosteen.SearchLongRoute.searchLongRoute;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-        // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("Hello and welcome!");
+        //路線図の情報
+        RailwayGraph graph = new RailwayGraph(new HashMap<>());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
+        //標準入力読取↑の関数に格納(parseIntの例外処理一切してないのちょっと怖い)
+        //今は、全要素入力後再度enterを入力してくれる想定
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String[] input = sc.nextLine().split(",");
+
+            //超適当
+            if (input.length != 3) break;
+
+            //情報入力
+            graph.addRail(Integer.parseInt(input[0].trim()), Integer.parseInt(input[1].trim()), Double.parseDouble(input[2].trim()));
+        }
+
+        //グラフ確認
+        //graph.printGraph();
+
+        for (String s : searchLongRoute(graph)) {
+            System.out.println(s);
         }
     }
 }
--- src/main/java/io/github/uttmangosteen/RailwayGraph.java
+++ src/main/java/io/github/uttmangosteen/RailwayGraph.java
@@ -1,20 +1,51 @@
 package io.github.uttmangosteen;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
-class RailGraph {
-    Map<Integer, StationData> stationMap;
-    RailGraph(Map<Integer, StationData> stationMap) {
+
+public class RailwayGraph {
+    Map<Integer, Set<Rail>> stationMap;
+
+    RailwayGraph(Map<Integer, Set<Rail>> stationMap) {
         this.stationMap = stationMap;
+    }
+
+    public void addRail(int stationID_1, int stationID_2, Double distance) {
+        addStation(stationID_1);
+        addStation(stationID_2);
+        //もとから長い経路がある場合は無視(最短経路探すときは符号逆)
+        if (stationMap.get(stationID_1).stream().anyMatch(rail ->
+                rail.destinationStationID == stationID_2 &&rail.distance >= distance)) return;
+        stationMap.get(stationID_1).add(new Rail(stationID_2, distance));
+        stationMap.get(stationID_2).add(new Rail(stationID_1, distance));
+    }
+
+    void addStation(int stationID) {
+        if (stationMap.containsKey(stationID)) return;
+        stationMap.put(stationID, new HashSet<>());
+    }
+
+    public void printGraph() {
+        for (int key : stationMap.keySet()) {
+            System.out.println(key + " -> " + stationMap.get(key));
+        }
     }
 }
 
-
-class Rail{
+class Rail {
     int destinationStationID;
     Double distance;
-    Rail(int destinationStationID, Double distance){
+
+    Rail(int destinationStationID, Double distance) {
         this.destinationStationID = destinationStationID;
         this.distance = distance;
     }
+
+    @Override
+    public String toString() {
+        return "(" + destinationStationID + ", " + distance + ")";
+    }
+
 }
