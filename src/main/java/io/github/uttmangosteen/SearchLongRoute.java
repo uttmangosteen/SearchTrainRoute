95fa6029fd2a7bf3682d8503c4d688b18aabd165
uttmangosteen <noisimportantpoint@gmail.com>
2025-08-26 04:02:34 +0900

    最長経路を解く関数を仮で宣言

---
+++ src/main/java/io/github/uttmangosteen/SearchLongRoute.java
@@ -0,0 +1,10 @@
+package io.github.uttmangosteen;
+
+public class SearchLongRoute {
+    public static String[] searchLongRoute(RailwayGraph graph){
+        String[] s = new String[2];
+        s[0] = "0";
+        s[1] = "1";
+        return s;
+    }
+}
